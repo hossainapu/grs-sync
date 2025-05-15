dd8f9448094530e1d00faaf03506f57695a8ec3a
Md. Musfikur Hasan Oli <musfikuroli@gmail.com>
2025-05-15 16:22:36 +0600

    Fix Config

--- src/main/java/com/istl/utils/DisableSSLCertificateCheckUtil.java
+++ src/main/java/com/istl/utils/DisableSSLCertificateCheckUtil.java
@@ -1,38 +1,31 @@
-/*    */ package com.istl.utils;
-/*    */ 
-/*    */ import java.io.IOException;
-/*    */ import java.net.URL;
-/*    */ import java.security.KeyManagementException;
-/*    */ import java.security.NoSuchAlgorithmException;
-/*    */ import javax.net.ssl.HostnameVerifier;
-/*    */ import javax.net.ssl.HttpsURLConnection;
-/*    */ import javax.net.ssl.SSLContext;
-/*    */ import javax.net.ssl.TrustManager;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class DisableSSLCertificateCheckUtil
-/*    */ {
-/*    */   public static void disableChecks() throws NoSuchAlgorithmException, KeyManagementException {
-/*    */     try {
-/* 18 */       (new URL("https://0.0.0.0/")).getContent();
-/* 19 */     } catch (IOException iOException) {}
-/*    */ 
-/*    */     
-/*    */     try {
-/* 23 */       SSLContext sslc = SSLContext.getInstance("TLS");
-/* 24 */       TrustManager[] trustManagerArray = { (TrustManager) new  Exception() };
-/* 25 */       sslc.init(null, trustManagerArray, null);
-/* 26 */       HttpsURLConnection.setDefaultSSLSocketFactory(sslc.getSocketFactory());
-/* 27 */       HttpsURLConnection.setDefaultHostnameVerifier((HostnameVerifier)new Exception());
-/* 28 */     } catch (Exception var2) {
-/* 29 */       var2.printStackTrace();
-/*    */     } 
-/*    */   }
-/*    */ }
+package com.istl.utils;
 
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
 
-/* Location:              D:\GRS Sync\grs-migrator-prod.jar!\BOOT-INF\classes\com\ist\\utils\DisableSSLCertificateCheckUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+public class DisableSSLCertificateCheckUtil {
+
+    public static void disableChecks() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        };
+
+        // Install the all-trusting trust manager
+        SSLContext sslc = SSLContext.getInstance("TLS");
+        sslc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslc.getSocketFactory());
+
+        // Install the all-trusting host name verifier
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+    }
+}
